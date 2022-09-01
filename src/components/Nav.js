a10652a940e833a8d34db1608c05616ea1a1ca2e
Madoga99 <111305071+Madoga99@users.noreply.github.com>
2022-09-01 09:23:09 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({ setLibraryStatus, libraryStatus }) => {
   
     return (
       <nav>
-        <h1>Musica Electronica</h1>
+        <h1>COUNTRY MUSIC</h1>
         <button
           className={libraryStatus ? "library-active" : ""}
           onClick={openLibraryHandler}
